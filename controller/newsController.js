de06b77ead0874305fd870530d8c633ee925ec44
unknown <abelputra101@gmail.com>
2025-04-25 17:29:34 +0700

    finsihed news controller

--- controller/newsController.js
+++ controller/newsController.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator')
+const { validationResult} = require('express-validator')
 const pool = require('../db/instance')
 const adminn = require('firebase-admin')
 
@@ -32,14 +32,14 @@ async function createNews(req, res){
         bannerBlobStream.on('finish', async() => {
             try{
                 await bannerFileUpload.makePublic()
-                const bannerPublic = `https://storage.googleapis.com/${storage.name}/${bannerFilename}`
+                const bannerPublic = `https://storage.googleapis.com/${storage.name}/${bannerFileName}`
 
                 const imageFileName = `News Image/${createdby}_${timestamp}_image`
                 const imageFileUpload = storage.file(imageFileName)
 
                 const imageBlobStream = imageFileUpload.createWriteStream({
                     metadata: {
-                        contentType: req.file.image[0].mimetype
+                        contentType: req.files.image[0].mimetype
                     },
                     resumable: false
                 })
@@ -51,11 +51,11 @@ async function createNews(req, res){
                 imageBlobStream.on('finish', async() => {
                     try{
                         await imageFileUpload.makePublic()
-                        const imagePublic = `https://storage.googleapis.com/${storage.name}/${imageFilename}`
+                        const imagePublic = `https://storage.googleapis.com/${storage.name}/${imageFileName}`
 
                         const newNews = await pool.query(
                             `INSERT INTO news (createdby, title, category, banner_url, image_url, content, summary)
-                            VALUES ($1, $2, $3, $4, $5, $6, $7),
+                            VALUES ($1, $2, $3, $4, $5, $6, $7)
                             RETURNING *`, [createdby, title, category, bannerPublic, imagePublic, content, summary]
                         )
 
@@ -92,7 +92,7 @@ async function saveNews(req, res) {
     try{
         await pool.query(
             `INSERT INTO savednews (uid, newsid)
-            VALUES $1, $2 RETURNING *`, [uid, newsid]
+            VALUES ($1, $2) RETURNING *`, [uid, newsid]
         )
         return res.status(201).json({ message: "News successfully saved"})
     }
@@ -101,10 +101,11 @@ async function saveNews(req, res) {
     }
 }
 
-async function homePageNews(res) {
+async function homePageNews(req, res) {
     try{
         const threeTop = await pool.query(
             `SELECT 
+                newsid,
                 banner_url, 
                 category, 
                 createdat::date AS created_date,
@@ -118,6 +119,7 @@ async function homePageNews(res) {
 
         const latestAll = await pool.query(
             `SELECT 
+                newsid,
                 banner_url,
                 category,
                 createdat::date AS created_date,
@@ -131,6 +133,7 @@ async function homePageNews(res) {
             `SELECT *
             FROM (
                 SELECT 
+                    n.newsid,
                     n.banner_url,
                     u.username,
                     n.category,
@@ -146,9 +149,9 @@ async function homePageNews(res) {
 
         return res.status(200).json({ 
             message: "Fetch successfull",
-            topNews: threeTop,
-            latestAll: latestAll,
-            latestCat: latestCategories
+            topNews: threeTop.rows,
+            latestAll: latestAll.rows,
+            latestCat: latestCategories.rows
         })
     }
     catch(error){
@@ -163,7 +166,8 @@ async function newsDetail(req, res) {
         return res.status(422).json({ message: "Invalid input, check your data" })
     }
 
-    const newsid = req.params
+    const { newsid } = req.params
+    const { uid } = req.body
 
     try{
         const newsDetail = await pool.query(
@@ -181,35 +185,272 @@ async function newsDetail(req, res) {
             ON n.createdby = u.uid
             WHERE newsid = $1`, [newsid]
         )
-        return res.status(200).json({ message: "Fetch sucessfull", detail: newsDetail})
+
+        const likeInfo = await pool.query(
+            `SELECT liked_status
+            FROM likednews
+            WHERE uid = $1 AND newsid = $2`, [uid, newsid]
+        )
+
+        let userLikeStatus = null;
+        if (likeInfo.rows.length > 0) {
+            userLikeStatus = likeInfo.rows[0].liked_status;
+        }
+
+        const result = {
+            ...newsDetail.rows[0],
+            like_status: userLikeStatus
+        }
+
+        return res.status(200).json({ message: "Fetch sucessfull", detail: result })
     }
     catch(error){
         return res.status(500).json({ message: error.message })
     }
 }
 
-async function allNews(res) {
+async function allNews(req, res) {
     
+    const category = req.params 
+    const { search = "" } = req.body
+
+    try{
+        let query = `
+        SELECT 
+            newsid,
+            banner_url,
+            title,
+            summary,
+            category,
+            createdat::date as created_date
+        FROM news`
+        const value = []
+
+        if( category !== "All" ){
+            query += ` WHERE category = $1`
+            value.push(category)
+        }
+
+        if(search){
+            if(category !== "All"){
+                query += ` AND title ILIKE $2`
+                value.push(`%${search}%`)
+            }
+            else{
+                query += ` AND title ILIKE $1`
+                value.push(`%${search}%`)
+            }
+        }
+
+        query += ` LIMIT 100`
+
+        const result = await pool.query(query, value)
+
+        return res.status(200).json({ message: "Fetch successfull", news: result.rows })
+    }
+    catch(error){
+        return res.status(500).json({ message: error.message })
+    }
 }
 
 async function savedNews(req, res) {
-    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({ message: "Invalid input, please check your data"})
+    }
+
+    const { uid } = req.params
+
+    try{
+        const savedNews = await pool.query(
+            `SELECT
+                n.newsid,
+                n.banner_url,
+                n.title,
+                n.summary,
+                n.category,
+                n.createdat::date as created_date
+            FROM news n
+            INNER JOIN savednews s
+            ON n.newsid = s.newsid
+            INNER JOIN users u
+            ON s.uid = u.uid
+            WHERE s.uid = $1`, [uid]
+        )
+
+        return res.status(200).json({ message: "Fetch successfull", savedNews: savedNews.rows})
+    }
+    catch(error){
+        return res.status(500).json({ message: error.message })
+    }
 }
 
 async function createdNews(req, res) {
-    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({ message: "Invalid input, please check your data" })
+    }
+
+    const { uid } = req.params
+
+    try{
+        const createdNews = await pool.query(
+            `SELECT
+                newsid,
+                banner_url,
+                title,
+                summary,
+                category,
+                createdat::date as created_date
+            FROM news
+            WHERE createdby = $1`, [uid]
+        )
+
+        return res.status(200).json({ message: "Fetch successfull", createdNews: createdNews.rows})
+    }
+    catch(error){
+        return res.status(500).json({ message: error.message })
+    }
 }
 
 async function changeLike(req, res) {
-    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({ message: "Invalid input, please check your data" })
+    }
+
+    const { uid, newsid, pressed } = req.body
+
+    try{
+        const likeStatus = await pool.query(
+            `SELECT like_status
+            FROM likednews
+            WHERE uid = $1 AND newsid = $2`, [uid, newsid]
+        )
+        if(likeStatus.rowCount === 0){
+            await pool.query(
+                `INSERT INTO likednews (uid, newsid, like_status)
+                VALUES ($1, $2, $3)`, [uid, newsid, pressed]
+            )
+            if(pressed === 'Like'){
+                await pool.query(
+                    `UPDATE news 
+                    SET likes = likes + 1
+                    WHERE newsid = $1`, [newsid]
+                )
+            }
+            else{
+                await pool.query(
+                    `UPDATE news 
+                    SET likes = likes - 1
+                    WHERE newsid = $1`, [newsid]
+                )
+            }
+        }
+        else{
+            const currentStatus = likeStatus.rows[0].like_status
+
+            if(currentStatus === pressed){
+
+                await pool.query(
+                    `DELETE FROM likednews
+                    WHERE uid = $1 AND newsid = $2`, [uid, newsid]
+                )
+
+                if(pressed === 'Like'){
+                    await pool.query(
+                        `UPDATE news
+                        SET likes = likes - 1
+                        WHERE newsid = $1`, [newsid]
+                    )
+                }
+
+                else{
+                    await pool.query(
+                        `UPDATE news
+                        SET likes = likes + 1
+                        WHERE newsid = $1`, [newsid]
+                    )
+                }
+            }
+            else{
+
+                await pool.query(
+                    `UPDATE likednews
+                    SET like_status = $1
+                    WHERE uid = $2 AND newsid = $3`, [pressed, uid, newsid]
+                )
+
+                if(pressed === 'Like'){
+                    await pool.query(
+                        `UPDATE news
+                        SET likes = likes + 2
+                        WHERE newsid = $1`, [newsid]
+                    )
+                }  
+
+                else{
+                    await pool.query(
+                        `UPDATE news
+                        SET likes = likes -2
+                        WHERE newsid = $1`, [newsid]
+                    )
+                }         
+            }
+        }
+        return res.status(200).json({ message: "Like status updated successfully" })
+    }
+    catch(error){
+        return res.status(500).json({ message: error.message })
+    }
 }
 
 async function deleteNews(req, res) {
-    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({ message: "Invalid input, please check your data" })
+    }
+
+    const { newsid } = req.params
+
+    try{
+        await pool.query(
+            `DELETE FROM news
+            WHERE newsid = $1`, [newsid]
+        )
+
+        return res.status(200).json({ message: "News successfully deleted" })
+    }
+    catch(error){
+        return res.status(500).json({ message: error.message })
+    }
 }
 
 async function unsaveNews(req, res) {
-    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({ message: "Invalid input, please check your data" })
+    }
+
+    const { uid, newsid } = req.body
+
+    try{
+        await pool.query(
+            `DELETE FROM savednews
+            WHERE uid = $1 AND newsid = $2
+            RETURNING *`, [uid, newsid]
+        )
+        
+        return res.status(200).json({ message: "News sunccessfully removed" })
+    }
+    catch(error){
+        return res.status(500).json({ message: error.message })
+    }
 }
 
 module.exports = {
@@ -217,4 +458,10 @@ module.exports = {
     saveNews,
     homePageNews,
     newsDetail,
+    allNews,
+    savedNews,
+    createdNews,
+    changeLike,
+    deleteNews,
+    unsaveNews
 }
